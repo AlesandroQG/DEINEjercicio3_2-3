fe28ba8737fc2c02283f2b8398c2f559efdc4718
AlesandroQG <alesandroquirosg@icloud.com>
2025-01-10 13:07:45 +0100

    Realizado informe de productos

--- src/main/java/com/alesandro/ejercicio3_23/controller/SupermercadoController.java
+++ src/main/java/com/alesandro/ejercicio3_23/controller/SupermercadoController.java
@@ -7,32 +7,20 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
 import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperCompileManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
 import net.sf.jasperreports.engine.util.JRLoader;
 import net.sf.jasperreports.view.JasperViewer;
 
-import java.net.URL;
 import java.sql.SQLException;
 import java.util.HashMap;
-import java.util.ResourceBundle;
 
 /**
  * Clase controladora de la vista principal de la aplicación
  */
-public class SupermercadoController implements Initializable {
-    /**
-     * Función que se ejecuta cuando se inicia la ventana
-     *
-     * @param location
-     * @param resources
-     */
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        //
-    }
-
+public class SupermercadoController {
     /**
      * Función que es ejecuta cuando se pulsa el botón “Abrir Informe” de la pestaña Productos. Abre el informe de productos
      *
@@ -61,10 +49,12 @@ public class SupermercadoController implements Initializable {
     @FXML
     void informeSecciones(ActionEvent event) {
         DBConnect connection;
+        HashMap<String, Object> parameters = new HashMap<String, Object>();
         try {
             connection = new DBConnect(); // Instanciar la conexión con la base de datos
+            parameters.put("productosinforme", compilar("SubinformeProductos.jrxml"));
             JasperReport report = (JasperReport) JRLoader.loadObject(SupermercadoApplication.class.getResource("reports/InformeProductos.jasper")); // Obtener el fichero del informe
-            JasperPrint jprint = JasperFillManager.fillReport(report, null, connection.getConnection()); // Cargar el informe con los secciones
+            JasperPrint jprint = JasperFillManager.fillReport(report, parameters, connection.getConnection()); // Cargar el informe con los secciones
             JasperViewer viewer = new JasperViewer(jprint, false); // Instanciar la vista del informe para mostrar el informe
             viewer.setVisible(true); // Mostrar el informe al usuario
         } catch (JRException | SQLException e) {
@@ -110,6 +100,22 @@ public class SupermercadoController implements Initializable {
         } catch (JRException | SQLException e) {
             System.err.println(e.getMessage());
             mostrarAlerta("Ha ocurrido un error cargando el informe");
+        }
+    }
+
+    /**
+     * Función que compila un subinforme para su uso en un informe
+     *
+     * @param informe a compilar
+     * @return informe compilado
+     */
+    public JasperReport compilar(String informe) {
+        try {
+            return JasperCompileManager.compileReport(SupermercadoApplication.class.getResourceAsStream("reports/" + informe)); // Compilar el informe
+        } catch (JRException e) {
+            System.err.println(e.getMessage());
+            mostrarAlerta("Ha ocurrido un error cargando el informe");
+            return null;
         }
     }
 
